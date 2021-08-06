e8d8207670c5cfa3394b57459610851bcb136a8f
lisellelogan <liselle.logan@yahoo.com>
2021-08-06 12:39:33 -0400

    add comments to plan application utilizing nav bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      // we want to display navbar
+      // default display to home page
+      // have component for each navbar item
+      utilize react router for navbar toggle
       <h1>We are in app.js!</h1>
     </div>
   );
