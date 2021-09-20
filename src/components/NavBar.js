72790daeb4c7a6280c5815d4b9feb2ea3a84531d
lisellelogan <liselle.logan@yahoo.com>
2021-09-20 18:22:38 -0400

    create nav item for navbar using Switch and Route

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Welcome from './Welcome.js';
 
 const NavBar = () => {
     return (
-        
+        <Switch>
+            <Route path="/welcome" exact component={Welcome} />
+        </Switch>
     )
 }
