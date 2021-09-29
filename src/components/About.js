fd009caac6b77be6cd5cff6e111eb0c829240ade
lisellelogan <liselle.logan@yahoo.com>
2021-09-29 18:53:21 -0400

    add content to about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,10 @@ import React from 'react';
 const About = () => {
     return (
         <>
-            <h1>About Page</h1>
+            <h1>About</h1>
+            <p>My name is Liselle and I’m a recent graduate from Flatiron School and I made the transition from medicine to tech.</p>
+            <p>Now you may be asking “why the transition into tech?”</p>
+            <p>Well although medicine wasn’t the right fit for me, I still have a deep passion for helping others. So during my time at Flatiron School, I always sought to create apps that helped our society whether it be for small businesses, mental health, and even our beloved pets! Creating these projects allowed me to combine my skills set and interests, and I’m highly interested in opportunities that not only better the company but also our society as a whole.</p>
         </>
     )
 }
