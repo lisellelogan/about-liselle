c957ccdd226e1ad1fdd58d9aaa7699508d9debb1
lisellelogan <liselle.logan@yahoo.com>
2021-09-29 18:27:37 -0400

    add buttons to download resume or learn more

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,6 +6,8 @@ const Welcome = () => {
             <h3>Hi, I'm</h3>
             <h1>Liselle Logan</h1>
             <p>Full-Stack Web Developer who has a passion of helping others through code!</p>
+
+            <button>Download Resume</button> <button>Learn More</button>
         </>
     )
 }
