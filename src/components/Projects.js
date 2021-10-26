9c2566d95c1757ee15daad399acc2e3948debfbb
lisellelogan <liselle.logan@yahoo.com>
2021-10-26 16:16:24 -0400

    add description to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,14 @@ const Projects = () => {
     return (
         <div>
             <h1>The Social Pup</h1>
+            <p>
+The Social Pup was created to help new dog owners, and old, find other pup's to have play dates! This application allows owner's to sign in as their dog and find other pup's to message via email.</p>
+            
             <h1>smallbizsociety.</h1>
+            <p>More than a handful of the local businesses in my neighborhood have gone out of business due to the impact of COVID-19 so I wanted to create something similar to Yelp but more niche for all small businesses in New York City. This app was created to serve as a platform that will allow a small business to add their business and start posting their products right away! The small business has the ability to create, edit, update, and delete their business as well as their items.</p>
+            
             <h1>Food for Fido</h1>
+            <p>Food For Fido is a web application that allows users to find dog friendly, healthy recipes for their pup. This web application has admin functionality in order to prevent users from creating "bad recipes" that do not fit the appropriate guidelines. The admin has the ability to create and delete recipes but cannot comment on recipes. The comment space is meant to be utilized by the users where they can create, rate, comment, edit, and delete comments on recipes.</p>
         </div>
     )
 }
