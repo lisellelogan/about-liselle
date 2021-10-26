c34f4b8a24ff0a4593083a023b420ff5234eba79
lisellelogan <liselle.logan@yahoo.com>
2021-10-26 16:23:45 -0400

    move project titles to center

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,16 +3,15 @@ import React from 'react';
 const Projects = () => {
     return (
         <div>
-            <h1>The Social Pup</h1>
-            <p>
-            The Social Pup was created to help new dog owners, and old, find other pup's to have play dates! This application allows owner's to sign in as their dog and find other pup's to message via email.</p>
+            <h1 align="center">The Social Pup</h1>
+            <p align="right">The Social Pup was created to help new dog owners, and old, find other pup's to have play dates! This application allows owner's to sign in as their dog and find other pup's to message via email.</p>
             <a href="https://www.youtube.com/watch?v=ovcXDkFOLi8"><img src="https://media.giphy.com/media/3GVw3yjlJGE6598Dbe/giphy.gif" alt="Giphy of The Social Pup"/></a><br/><br/>
             
-            <h1>smallbizsociety.</h1>
+            <h1 align="center">smallbizsociety.</h1>
             <p>More than a handful of the local businesses in my neighborhood have gone out of business due to the impact of COVID-19 so I wanted to create something similar to Yelp but more niche for all small businesses in New York City. This app was created to serve as a platform that will allow a small business to add their business and start posting their products right away! The small business has the ability to create, edit, update, and delete their business as well as their items.</p>
             <a href="https://www.youtube.com/watch?v=xaufc73i7yY"><img src="https://media.giphy.com/media/6E6H9JomKzN1SQyckI/giphy.gif" alt="Giphy of smallbizsociety."/></a><br/><br/>
             
-            <h1>Food for Fido</h1>
+            <h1 align="center">Food for Fido</h1>
             <p>Food For Fido is a web application that allows users to find dog friendly, healthy recipes for their pup. This web application has admin functionality in order to prevent users from creating "bad recipes" that do not fit the appropriate guidelines. The admin has the ability to create and delete recipes but cannot comment on recipes. The comment space is meant to be utilized by the users where they can create, rate, comment, edit, and delete comments on recipes.</p>
             <a href="https://www.youtube.com/watch?v=xLypFHFCnTQ"><img src="https://media.giphy.com/media/NqrPc6nfY1F1xwPRUx/giphy.gif" alt="Giphy of Food for Fido"/></a>
 
