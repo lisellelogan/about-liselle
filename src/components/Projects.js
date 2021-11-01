ea2f3a5871e55b341dbc54af37f4d15573d47f05
lisellelogan <liselle.logan@yahoo.com>
2021-11-01 16:38:39 -0400

    add flex container to smallbizsociety

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,8 +10,10 @@ const Projects = () => {
             </div>
             
             <h1 align="center">smallbizsociety.</h1>
-            <p>More than a handful of the local businesses in my neighborhood have gone out of business due to the impact of COVID-19 so I wanted to create something similar to Yelp but more niche for all small businesses in New York City. This app was created to serve as a platform that will allow a small business to add their business and start posting their products right away! The small business has the ability to create, edit, update, and delete their business as well as their items.</p>
-            <a href="https://www.youtube.com/watch?v=xaufc73i7yY"><img src="https://media.giphy.com/media/6E6H9JomKzN1SQyckI/giphy.gif" alt="Giphy of smallbizsociety."/></a><br/><br/>
+            <div display="flex-container">
+                <p>More than a handful of the local businesses in my neighborhood have gone out of business due to the impact of COVID-19 so I wanted to create something similar to Yelp but more niche for all small businesses in New York City. This app was created to serve as a platform that will allow a small business to add their business and start posting their products right away! The small business has the ability to create, edit, update, and delete their business as well as their items.</p>
+                <a href="https://www.youtube.com/watch?v=xaufc73i7yY"><img src="https://media.giphy.com/media/6E6H9JomKzN1SQyckI/giphy.gif" alt="Giphy of smallbizsociety."/></a><br/><br/>
+            </div>
             
             <h1 align="center">Food for Fido</h1>
             <p>Food For Fido is a web application that allows users to find dog friendly, healthy recipes for their pup. This web application has admin functionality in order to prevent users from creating "bad recipes" that do not fit the appropriate guidelines. The admin has the ability to create and delete recipes but cannot comment on recipes. The comment space is meant to be utilized by the users where they can create, rate, comment, edit, and delete comments on recipes.</p>
