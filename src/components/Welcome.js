b0462c08bf4b6b704a37e876cb578862bef0f728
lisellelogan <liselle.logan@yahoo.com>
2021-11-01 15:58:21 -0400

    assigning function variable and move into event handler

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 const Welcome = () => {
-
     const history = useHistory();
-    const handleClick = () => history.push('/about');
-
     return(
         <>
             <h3>Hi, I'm</h3>
             <h1>Liselle Logan</h1>
             <p>Full-Stack Software Engineer</p>
 
-            <button>Download Resume</button> <button onClick={handleClick}>Learn More</button>
+            <button>Download Resume</button> <button onClick={() => history.push('/about')}>Learn More</button>
         </>
     )
 }
