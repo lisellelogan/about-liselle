fb86e2ffbc7fca399e9e4458027278a3888f53e5
lisellelogan <liselle.logan@yahoo.com>
2021-11-16 22:15:24 -0500

    set up styling for contact page

--- src/components/NavBarStyle.js
+++ src/components/NavBarStyle.js
@@ -1,34 +1,50 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBarStyle = () => {
-    return (
-        <div>
-        <NavLink 
-            to="/welcome"
-            activeStyle={{
-                fontWeight: "bold"
-            }}
-            >Welcome
-        </NavLink><br/>
+  return (
+    <div>
+      <NavLink
+        to="/welcome"
+        activeStyle={{
+          fontWeight: "bold",
+        }}
+      >
+        Welcome
+      </NavLink>
+      <br />
 
-        <NavLink 
-            to="/about"
-            activeStyle={{
-                fontWeight: "bold"
-            }}
-            >About
-        </NavLink><br/>
+      <NavLink
+        to="/about"
+        activeStyle={{
+          fontWeight: "bold",
+        }}
+      >
+        About
+      </NavLink>
+      <br />
 
-        <NavLink 
-            to="/projects"
-            activeStyle={{
-                fontWeight: "bold"
-            }}
-            >Projects
-        </NavLink><br/>
+      <NavLink
+        to="/projects"
+        activeStyle={{
+          fontWeight: "bold",
+        }}
+      >
+        Projects
+      </NavLink>
+      <br />
+
+      <NavLink
+        to="/contact"
+        activeStyle={{
+          fontWeight: "bold",
+        }}
+      >
+        Contact
+      </NavLink>
+      <br />
     </div>
-    )
-}
+  );
+};
 
 export default NavBarStyle;
