95350b516bc1af950bafa3d27cb7a71bef307837
lisellelogan <liselle.logan@yahoo.com>
2021-11-16 22:13:22 -0500

    export contact page and import in navbar

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,3 +3,5 @@ import React from "react";
 const Contact = () => {
   return <h1>Contact Page</h1>;
 };
+
+export default Contact;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import NavBarStyle from './NavBarStyle.js';
-import Welcome from './Welcome.js';
-import About from './About.js'
-import Projects from './Projects';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBarStyle from "./NavBarStyle.js";
+import Welcome from "./Welcome.js";
+import About from "./About.js";
+import Projects from "./Projects";
+import Contact from "./Contact.js";
 
 const NavBar = () => {
-    return (
-        <>
-            <NavBarStyle />
-            <Switch>
-                <Route path="/welcome" exact component={Welcome} />
-                <Route  path="/about" exact component={About}/>
-                <Route path="/projects" exact component={Projects}/>
-            </Switch>
-        </>
-    )
-}
+  return (
+    <>
+      <NavBarStyle />
+      <Switch>
+        <Route path="/welcome" exact component={Welcome} />
+        <Route path="/about" exact component={About} />
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/contact" exact component={Contact} />
+      </Switch>
+    </>
+  );
+};
 
 export default NavBar;
