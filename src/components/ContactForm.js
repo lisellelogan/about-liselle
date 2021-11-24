56bf3acdecc4ecabbea398edffe158213de25cec
lisellelogan <liselle.logan@yahoo.com>
2021-11-24 10:45:24 -0500

    add render() since it is class

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
 class ContactForm extends Component {
-  return (
-    <form>
-      <input type="text" name="firstName" value />
-      <input />
-      <input />
-    </form>
-  );
-};
+  render() {
+    return (
+      <form>
+        <input type="text" name="firstName" value />
+        <input />
+        <input />
+      </form>
+    );
+  }
+}
 
 export default ContactForm;
