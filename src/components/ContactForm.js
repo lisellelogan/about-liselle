5c7fbb1a5b35e2215072d8be417658c2365288cb
lisellelogan <liselle.logan@yahoo.com>
2021-11-24 10:48:21 -0500

    add state

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 class ContactForm extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  };
+
   render() {
     return (
       <form>
